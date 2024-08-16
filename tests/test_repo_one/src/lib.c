f00b05cade40e5e456d79eff3bb9b7e787f2e8f7
ZSR3004 <zrahman3004@gmail.com>
2024-08-16 17:38:41 -0500

    created first test

---
+++ tests/test_repo_one/src/lib.c
@@ -0,0 +1,6 @@
+#include "../include/lib.h"
+
+void test_function (void) {
+    printf("This is a test!\n");
+    return;
+}
---
+++ tests/test_repo_one/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "../include/lib.h"
+
+int main(void) {
+
+    test_function();
+    return 0;
+
+}
