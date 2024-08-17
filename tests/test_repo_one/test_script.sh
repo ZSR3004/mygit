5e221ff58ec3b593ab2a4b33b8b4a4d5dd4c1f42
ZSR3004 <zrahman3004@gmail.com>
2024-08-17 09:33:32 -0500

    added new test

--- tests/test_repo_one/test_script.sh
+++ tests/test_repo_one/test_script.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+ mygit=./../../bin/mygit
+
 if [ "$1" = "clean" ]; then
     rm -rf .mygit
 elif [ "$1" = "1" ]; then
     rm -rf .mygit
-    mygit=./../../bin/mygit
     $mygit init
     $mygit add src/main.c
+elif [ "$1" = "2" ]; then
+    rm -rf .mygit
+    $mygit init
+    $mygit add .
+else
+    echo "Invalid argument"
 fi
