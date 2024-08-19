59184484a424676ee3bd85fc6510cf8e2aaf210b
ZSR3004 <zrahman3004@gmail.com>
2024-08-18 21:45:02 -0500

    added test for subdir handling of mygit add

--- tests/test_script.sh
+++ tests/test_script.sh
@@ -3,15 +3,16 @@
  mygit=./../../bin/mygit
 
 if [ "$1" = "clean" ]; then
-    rm -rf .mygit
+    rm -rf /home/zsr/curr_proj/mygit/tests/test_repo_one/.mygit
 elif [ "$1" = "1" ]; then
     rm -rf .mygit
     $mygit init
     $mygit add /src/main.c
 elif [ "$1" = "2" ]; then
     rm -rf .mygit
+    cd test_repo_one
     $mygit init
-    $mygit add /home/zsr/curr_proj/mygit/tests/test_repo_one/src
+    $mygit add /home/zsr/curr_proj/mygit/tests/test_repo_one
 else
     echo "Invalid argument"
 fi
