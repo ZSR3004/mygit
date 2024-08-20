0011a846f39eaf028ab7a1b59fc4efee314be581
ZSR3004 <zrahman3004@gmail.com>
2024-08-20 00:46:15 -0500

    added error handling to main.c

--- src/main.c
+++ src/main.c
@@ -12,22 +12,28 @@
 
 int main(int argc, char *argv[]) {
 
-    if (argc == 1) { // used for debugging at the moment; usually takes you to the general help menu
-        gen_help();
-    } else if (argc > 1) {
+    if (argc == 1) gen_help();
+    else if (argc > 1) {
+
         char command[100];
         strcpy(command, argv[1]);
+
         if (strcmp(command, "init") == 0) {
             init_mygit_directory();
         } else if (strcmp(command, "add") == 0) {
             index_cases(argv[2]);
         } else if (strcmp(command, "commit") == 0) {
             //code
-        } else if (strcmp(command, "log") == 0) {
+        } else if (strcmp(command, "branch") == 0) {
+            // code
+        } else if (strcmp(command, "checkout") == 0) {
             // code
         } else {
-            // code
+            printf("Invalid command\n");
         }
+
+        free(command);
+
     }
 
     return 0;
