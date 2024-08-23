a3ac2257b941738bfb0027d80cd0005dc82aa943
ZSR3004 <zrahman3004@gmail.com>
2024-08-23 08:51:58 -0500

    updated gitignore and created a commit command header file

---
+++ include/commit.h
@@ -0,0 +1,25 @@
+/**
+ * @file commit.h
+ * @brief Header for the "mygit commit" command.
+ * 
+ * The "mygit commit" command is used after staging changes with the "mygit add" command. This
+ * command sets all stages in stone by referencing the index file. The command does two things:
+ *      1. Creates a tree object based on the index file.
+ *      2. Creates a commit object (commitObj) to store metadata about the commit.
+ * 
+ * To learn more about these object types see objects.h and objects.c. To learn more about the
+ * index file see staging.h, staging.c, add.h, and add.c.
+ * 
+ * The "commit" command can be written with a flag called the message flag. This flag is written
+ * after "mygit commit" as either "--message" or "-m" for short. This flag indicates that the next
+ * string (staring and ending with a single quotation mark) is the commit message. This operates
+ * largely similar to Git's commit messages and can be used by someone to go back through commits
+ * and see the purpose of each commit.
+ * 
+ * @author Ziyad Rahman
+ * 
+ */
+
+void commit_changes(char *commit_message);
+
+char *extract_message(char *argv[]);
