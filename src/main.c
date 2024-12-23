910dd2a97ca788d7f0967776cce22d01f37131fc
ZSR3004 <zrahman3004@gmail.com>
2024-12-23 11:48:27 +0400

    setup refactor branch

--- src/main.c
+++ src/main.c
@@ -3,40 +3,12 @@
 #include <stdbool.h>
 #include <string.h>
 
-#include "../include/help.h"
-#include "../include/file.h"
-#include "../include/init.h"
-#include "../include/hash.h"
-#include "../include/add.h"
-#include "../include/strarr.h"
-#include "../include/tree_cache.h"
-#include "../include/tree.h"
-#include "../include/commit.h"
-
 int main(int argc, char *argv[]) {
 
     if (argc == 1) {
 
-        commit_changes("This is my message");
-        
     } else if (argc > 1) {
-
-        char command[100];
-        strcpy(command, argv[1]);
-
-        if (strcmp(command, "init") == 0) {
-            init_mygit_directory();
-        } else if (strcmp(command, "add") == 0) {
-            add_command_cases(argv[2]);
-        } else if (strcmp(command, "commit") == 0) {
-            //code
-        } else if (strcmp(command, "branch") == 0) {
-            // code
-        } else if (strcmp(command, "checkout") == 0) {
-            // code
-        } else {
-            printf("Invalid command\n");
-        }
+ 
     }
 
     return 0;
