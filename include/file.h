c200c2e794445e6fa0170973c04b72bacd1fae08
ZSR3004 <zrahman3004@gmail.com>
2024-12-23 12:47:10 +0400

    created base for file handlers

---
+++ include/file.h
@@ -0,0 +1,38 @@
+/**
+ * @file.h
+ * @brief Handles file input/output
+ * 
+ * file.h handles file input, output, and creation. It's primary purpose is to error handle
+ * for possibly finicky file functions like fopen and fwrite. It also handles writing to files
+ * and extracting what are in files and placing that text into strings.
+ * 
+ * @author Ziyad Rahman
+ * 
+ */
+
+#ifndef FILE_H
+#define FILE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+
+DIR *create_directory(const char *path, mode_t mode);
+DIR* open_directory(const char *path);
+void close_directory(DIR *dir);
+
+FILE *create_file(const char *path, mode_t mode);
+FILE *open_file(const char *path, int flags);
+void close_file(int fd);
+
+char *get_cwd(void);
+
+unsigned long get_file_size(FILE *file);
+size_t copy_file_to_string(char *file_contents, long unsigned file_size, FILE *file);
+char *get_file_text(FILE *file);
+
+#endif // FILE_H
--- src/file.c
+++ src/file.c
@@ -1,7 +1,35 @@
-#include <stdio.h>
+#include "../include/file.h"
 
-void open_directory() {
-
+DIR *create_directory(const char *path, mode_t mode) {
+    
+}
+DIR* open_directory(const char *path) {
+    
+}
+void close_directory(DIR *dir) {
+    
 }
 
-void 
+FILE *create_file(const char *path, mode_t mode) {
+    
+}
+FILE *open_file(const char *path, int flags) {
+    
+}
+void close_file(int fd) {
+    
+}
+
+char *get_cwd(void) {
+    
+}
+
+unsigned long get_file_size(FILE *file) {
+    
+}
+size_t copy_file_to_string(char *file_contents, long unsigned file_size, FILE *file) {
+    
+}
+char *get_file_text(FILE *file) {
+    
+}
